e763375950274ee8103aa129ae56ac00ac2544ae
William <williamlsh@protonmail.com>
2025-09-03 16:14:46 -0500

    Minor update

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,8 +3,8 @@
 set -e
 
 EMAIL="williamlsh@protonmail.com"
-MOLD_VERSION="2.37.1"
-HELIX_VERSION="25.01.1"
+MOLD_VERSION="2.40.4"
+HELIX_VERSION="25.07.1"
 
 sudo apt-get update && sudo apt-get upgrade -y
 
